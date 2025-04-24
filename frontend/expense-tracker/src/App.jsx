519d290c6cd14beaaa94339771ab66b61fe69b10
Javae Vittoni <javaevittoni@gmail.com>
2025-04-24 14:27:31 -0700

    define app routes

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
+import Income from './pages/Dashboard/Income';
+import Home from './pages/Dashboard/Home';
+import Expense from './pages/Dashboard/Expense';
 
 const App = () => {
   return (
     <div>
-      
+      <Router>
+        <Routes>
+          <Route path='/' element={<Root />} />
+          <Route path='/login' exact element={<Login />} />
+          <Route path='/signUp' exact element={<SignUp />} />
+          <Route path='/dashboard' exact element={<Home />} />
+          <Route path='/income' exact element={<Income />} />
+          <Route path='/expense' exact element={<Expense />} />
+
+
+        </Routes>
+      </Router>
     </div>
   )
 }
 
-export default App
+export default App;
+
+const Root = () => {
+  // Check if token exists in local storage
+  const isAuhenticated = !!localStorage.getItem("token");
+
+  // Redirect to dashboard if authenticated, otherwise to login
+  return isAuhenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  );
+};
